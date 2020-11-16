8c04bb5b9f734de705565c4a7b62cfa4a8fd5179
Rishabh Shrivastava <rshrivastav@BLR.ALLEGISINDIA.COM>
2020-11-16 10:48:45 +0530

    new changes

--- createJobs.groovy
+++ createJobs.groovy
@@ -7,15 +7,15 @@ pipelineJob('pipelineJob') {
     }
 }
 
-pipelineJob('theme-park-job') {
+pipelineJob('nse-api-job') {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://gitlab.blr.teksystems.com/interns2020/campus-training.git'
                     }
-                    branch 'master'
+                    branch 'develop'
                 }
             }
         }
